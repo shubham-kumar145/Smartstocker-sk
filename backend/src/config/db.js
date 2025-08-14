895e9e3d8dec878a8ce0662ed519166bf18106c1
shubham-kumar145 <shubhamkr5923@gmail.com>
2025-08-14 22:37:35 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
 async function main(){
-    await mongoose.connect("mongodb+srv://shubhamkr5923:5923%40S.kumar@shubhamdata.ougm4ij.mongodb.net/Inventory");
+    await mongoose.connect("mongodb+sb.net/Inventory");
     console.log("db connected");
     
 }
 
 module.exports = main
+
 
 
